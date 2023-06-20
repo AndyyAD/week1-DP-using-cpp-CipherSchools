1acc29c162539a6988e337da7048099c607d3a5d
Andy <ashishdeb1231@gmail.com>
2023-06-20 11:27:45 +0530

    Completed lecture 3

---
+++ Lecture 3.cpp
@@ -0,0 +1,20 @@
+// Rotating a matrix by 90 deg 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix){
+    for (int i=0; i<matrix.size(); i++){
+        for (int j=0; j<=i; j++){
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for(int i=0; i<matrix.size(); i++){
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    
+    return 0;
+}
