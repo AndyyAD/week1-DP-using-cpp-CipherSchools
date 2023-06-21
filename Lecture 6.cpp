eb0ebfd91f54a5bf3683e9eb7a7927ee30c121a8
Andy <ashishdeb1231@gmail.com>
2023-06-21 09:46:16 +0530

    Lecture 6 1st question

---
+++ Lecture 6.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Sort 012
+
+// vector<int> sortedArray(vector<int>& nums) {
+//     int n = nums.size();
+//     int p0 = 0, p1 = 0, p2 = n-1;
+//     while (p1<=p2){
+//         if(nums[p1] == 0){
+//             swap(nums[p1], nums[p0]);
+//             p1++;
+//             p0++;
+//         } else if (nums[p1] == 2) {
+//             swap(nums[p1], nums[p2]);
+//             p2--;
+//         } else {
+//             p1++;
+//         }
+//     }
+//     return nums;
+// }
+
+int main() {
+    return 0;
+}
+
