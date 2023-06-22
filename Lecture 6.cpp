b8e9d7a720bdf977a1888244cdb236a1c9e69fa6
Andy <ashishdeb1231@gmail.com>
2023-06-22 05:52:12 +0530

    Lecture 6 completed

--- Lecture 6.cpp
+++ Lecture 6.cpp
@@ -21,6 +21,47 @@ using namespace std;
 //     return nums;
 // }
 
+// set matrix zero
+
+// void setZeroes(vector<vector<int>>& matrix) {
+//     int m = matrix.size();
+//     int n = matrix[0].size();
+//     vector<int> rows(m, 0);
+//     vector<int> columns(n, 0);
+//     for (int i=0; i<m; i++){
+//         for (int j=0; j<n; j++){
+//             if (matrix[i][j] == 0){
+//                 rows[i] = 1;
+//                 columns[j] = 1;
+//             }
+//         }
+//     }
+//     for(int i=0; i<m; i++){
+//         for(int j=0; j<n; j++){
+//             if(rows[i] == 1 || columns[j] == 1){
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+// }
+
+// search a 2d matrix ii
+
+// bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//     int m = 0;
+//     int n = matrix[0].size()-1;
+//     while (m<=matrix.size()-1 and n>=0){
+//         if(target == matrix[m][n]){
+//             return true;
+//         } else if (target < matrix[m][n]){
+//             n--;
+//         } else if (target > matrix[m][n]){
+//             m++;
+//         }
+//     }
+//     return false;
+// }
+
 int main() {
     return 0;
 }
