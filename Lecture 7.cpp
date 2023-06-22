6da9cdc72d6cb0348e5b984c1ea4b0a87dad8554
Andy <ashishdeb1231@gmail.com>
2023-06-22 09:27:18 +0530

    3 sum

--- Lecture 7.cpp
+++ Lecture 7.cpp
@@ -1,32 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int search(vector<int>& nums, int target) {
-    int start=0,end=nums.size()-1;
-    int mid= (start+end)/2;
-    while(start<=end){
-        mid=(start+end)/2;
-        if(target==nums[mid]){
-            return mid;
+//search in a sorted rotated array
+
+// int search(vector<int>& nums, int target) {
+//     int start=0,end=nums.size()-1;
+//     int mid= (start+end)/2;
+//     while(start<=end){
+//         mid=(start+end)/2;
+//         if(target==nums[mid]){
+//             return mid;
+//         }
+//         if(nums[start]<=nums[mid]){
+//             if(nums[start]<=target && nums[mid]>=target){
+//                 end=mid-1;
+//             }
+//             else{
+//                 start=mid+1;
+//             }
+//         }
+//         else{
+//             if(nums[end]>=target && nums[mid]<=target){
+//                 start=mid+1;
+//             }
+//             else{
+//                 end=mid-1;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// 3 SUM problem
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+        if(nums.size() < 3){
+            return {};
         }
-        if(nums[start]<=nums[mid]){
-            if(nums[start]<=target && nums[mid]>=target){
-                end=mid-1;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for (int i=0; i<nums.size()-2; i++){
+            int curr = nums[i];
+            int start = i+1, end = nums.size()-1;
+            while(start < end){
+                int mid = start + (end - start) / 2;
+                if(curr + nums[start] + nums[end] == 0){
+                    ans.push_back({curr, nums[start], nums[end]});
+                    start++; end--;
+                    while(start<end and nums[start - 1] == nums[start]){
+                        start++;
+                    }
+                    while(start<end and  nums[end] == nums[end + 1]){
+                        end--;
+                    }
+                } else if (curr + nums[start] + nums[end] < 0){
+                    start++;
+                } else {
+                    end--;
+                }
             }
-            else{
-                start=mid+1;
+            while (i< nums.size() - 1 and nums[i] == nums[i+1]){
+                i++;
             }
         }
-        else{
-            if(nums[end]>=target && nums[mid]<=target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-        }
-    }
-    return -1;
+        return ans;
 }
 
 int main() {
