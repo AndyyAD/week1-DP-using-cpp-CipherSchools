ca2230498201b1c2bbea9427f8fb48771d3dddb5
Andy <ashishdeb1231@gmail.com>
2023-06-28 10:03:56 +0530

    completed lecture 10

--- Lecture 10.cpp
+++ Lecture 10.cpp
@@ -37,6 +37,48 @@ using namespace std;
 //     }
 // }
 
+// power(x,n)
+
+// double myPow(double x, int n) {
+//     if(n==0){
+//         return 1;
+//     }
+//     if(n==1){
+//         return x;
+//     }
+//     if(n>0){
+//         if(n%2==0){
+//             return myPow(x*x,n/2);
+//         } else {
+//             return x*myPow(x*x,n/2);
+//         }
+//     } else {
+//         n=abs(n);
+//         if(n%2==0) {
+//             return 1/(myPow(x*x,n/2));
+//         } else {
+//             return 1/(x*myPow(x*x,n/2));
+//         }
+//     }        
+// }
+
+// powerset (teacher's method)
+
+vector<vector<int>> power(vector<int>& nums){
+    vector<vector<int>> TotalSubsets;
+    int n=nums.size();
+    for(int num=0;num<=pow(2,n)-1;num++){
+        vector<int> sub;
+        for(int i=0;i<=n-1;i++){
+            if(num&(1<<i)){
+                sub.push_back(nums[i]);
+            }
+        }
+        TotalSubsets.push_back(sub);
+    }
+    return TotalSubsets;
+}
+
 int main() {
     
     return 0;
