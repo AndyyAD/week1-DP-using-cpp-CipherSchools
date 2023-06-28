21f5f9bb2332412e1cd0b5a6ea35e3a67a75cca2
Andy <ashishdeb1231@gmail.com>
2023-06-28 10:44:07 +0530

    Completed lecture 11

--- Lecture 11.cpp
+++ Lecture 11.cpp
@@ -1,41 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isSafe(vector<vector<char>> &board, int row, int col, char value){
-    int n = board.size();
+// valid sudoku
 
-    for (int i = 0; i < n; i++){
-        if (board[row][i] == value){
-            return false;
-        }
-        if (board[i][col] == value){
-            return false;
-        }
-        if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value){
-            return false;
-        }
-    }
-    return true;
-}
+// bool isSafe(vector<vector<char>> &board, int row, int col, char value){
+//     int n = board.size();
 
-bool isValidSudoku(vector<vector<char>> &board) {
-    int n = board.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (board[i][j] == '.') {
-                for (char value = '1'; value <= '9'; value++) {
-                    if (isSafe(board, i, j, value)) {
-                        board[i][j] = value;
-                        bool nextsolution = isValidSudoku(board);
-                        if (nextsolution == true)
-                            return true;
-                        else
-                            board[i][j] = '.';
-                    }
-                }
-                return false;
+//     for (int i = 0; i < n; i++){
+//         if (board[row][i] == value){
+//             return false;
+//         }
+//         if (board[i][col] == value){
+//             return false;
+//         }
+//         if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// bool isValidSudoku(vector<vector<char>> &board) {
+//     int n = board.size();
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             if (board[i][j] == '.') {
+//                 for (char value = '1'; value <= '9'; value++) {
+//                     if (isSafe(board, i, j, value)) {
+//                         board[i][j] = value;
+//                         bool nextsolution = isValidSudoku(board);
+//                         if (nextsolution == true)
+//                             return true;
+//                         else
+//                             board[i][j] = '.';
+//                     }
+//                 }
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+//knight tour (my sol)
+
+bool checkValidGrid(vector<vector<int>>& grid) {
+    int dir[8][2] = {{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}};
+    int count = 0, maxCount = grid.size() * grid.size()-1;
+    int x = 0, y = 0, i, j;
+    if(grid[x][y] != count) return false;
+    while(count < maxCount){
+        count++;
+        int flag = 1;
+        for(auto d: dir){
+            i = x + d[0]; j = y + d[1];
+            if(i >= 0 && j >= 0  && i < grid.size() && j < grid.size() && grid[i][j] == count){ 
+                x = i; y = j; flag = 0; break;
             }
-        }
+        } 
+        if(flag) return false;
     }
     return true;
 }
